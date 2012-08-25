e84ef7991b85248d87f545a0768c7643f6c69ba2
Alex Schearer <aschearer@gmail.com>
2012-08-25 22:45:59 +0200

    Animating movement of doodads

--- LudumDare24/Views/Doodads/DoodadView.cs
+++ LudumDare24/Views/Doodads/DoodadView.cs
@@ -1,3 +1,4 @@
+using System;
 using LudumDare24.Models;
 using LudumDare24.Models.Doodads;
 using Microsoft.Xna.Framework;
@@ -12,6 +13,8 @@ namespace LudumDare24.Views.Doodads
         private Texture2D texture;
         private Vector2 origin;
         private Vector2 position;
+        private TimeSpan movementTimer;
+        private float targetRunTime;
 
         public DoodadView(IDoodad doodad)
         {
@@ -29,13 +32,38 @@ namespace LudumDare24.Views.Doodads
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            this.position = new Vector2(
+            Vector2 targetPosition = new Vector2(
                 Constants.TileSize * doodad.Column + Constants.TileHalfSize,
                 Constants.TileSize * doodad.Row + Constants.TileHalfSize);
 
+            Vector2 currentPosition = this.position;
+            if (targetPosition != this.position && this.movementTimer == TimeSpan.Zero)
+            {
+                this.targetRunTime = ((targetPosition - this.position).Length() / Constants.TileSize) * 0.1f;
+                this.movementTimer = TimeSpan.FromSeconds(this.targetRunTime);
+            }
+
+            if (this.movementTimer > TimeSpan.Zero)
+            {
+                if (this.movementTimer > gameTime.ElapsedGameTime)
+                {
+                    this.movementTimer -= gameTime.ElapsedGameTime;
+                    currentPosition = Vector2.Lerp(
+                        this.position,
+                        targetPosition,
+                        (float)(1 - (this.movementTimer.TotalSeconds / this.targetRunTime)));
+                }
+                else
+                {
+                    this.movementTimer = TimeSpan.Zero;
+                    this.position = targetPosition;
+                    currentPosition = targetPosition;
+                }
+            }
+
             spriteBatch.Draw(
                 this.texture,
-                this.position,
+                currentPosition,
                 null,
                 Color.White,
                 0,
